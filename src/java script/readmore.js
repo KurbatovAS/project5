09d59480edfd24340f0676f92a3a294dea5f9600
Sofia Protsiv <prochivsofia@gmail.com>
2021-09-13 09:27:08 -0400

    add js for 3 button

--- src/java script/readmore.js
+++
@@ -1,7 +0,0 @@
-
-// const more = document.querySelectorAll(".product-card__button");
-// for (const i = 0; i<more.length; i++){
-//     more[i].addEventListener('click',function(){
-//         more[i].parentNode.classList.toggle('active')
-//     })
-// }
