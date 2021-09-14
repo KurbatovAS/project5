69d5af7080e6165d68ac140c1f9dbc838afa8b8a
KurbatovAS <85691791+KurbatovAS@users.noreply.github.com>
2021-09-14 16:39:08 +0300

    Delete src/java script directory

--- src/java script/readmore-active.js
+++
@@ -1,7 +0,0 @@
-
-const more = document.querySelectorAll("#myBtn");
-for (const i = 0; i<more.length; i++){
-    more[i].addEventListener('click',function(){
-        more[i].parentNode.classList.toggle('active')
-    })
-}
